56f66f848c84dab179661e2d0435192672839722
KiloBites <kiloktane@gmail.com>
2024-02-09 23:55:33 -0500

    Added more SFX

--- Assets/ModScripts/SetupWizardScript.cs
+++ Assets/ModScripts/SetupWizardScript.cs
@@ -16,6 +16,8 @@ public class SetupWizardScript : MonoBehaviour {
 	public KMSelectable[] mainButtons, keyboardLetters, keyboardNumbers, folderButtons, accountPrompts, page2Buttons;
 	public KMSelectable backSpace, shift, reset, submit;
 
+	public TextMesh[] mainTexts, expressionDisplays; 
+
 	public GameObject window;
 	public GameObject[] pages;
 
@@ -44,7 +46,7 @@ public class SetupWizardScript : MonoBehaviour {
 	private Expression[] generatedPuzzle, modifiedPuzzle;
 	private EquationSystem equationSystem;
 
-	private int currentPos;
+	private int currentPos, currentPage = 0, page2Ix = 0;
 
 	private string usernameInput, passwordInput, passwordAsterisk;
 
@@ -131,6 +133,23 @@ public class SetupWizardScript : MonoBehaviour {
 		foreach (KMSelectable number in keyboardNumbers)
 			number.OnInteract += delegate () { KeyboardNumberPress(number); return false; };
 
+		foreach (KMSelectable folder in folderButtons)
+			folder.OnInteract += delegate () { FolderPress(folder); return false; };
+
+		foreach (KMSelectable mainButton in mainButtons)
+			mainButton.OnInteract += delegate () { MainButtonPress(mainButton); return false; };
+
+		foreach (KMSelectable prompt in accountPrompts)
+			prompt.OnInteract += delegate () { AccountPromptPress(prompt); return false; };
+
+
+
+		reset.OnInteract += delegate () { ResetPress(); return false; };
+		backSpace.OnInteract += delegate () { BackSpacePress(); return false; };
+		shift.OnInteract += delegate () { ShiftPress(); return false; };
+		submit.OnInteract += delegate () { SubmitPress(); return false; };
+
+
 		Module.OnActivate += delegate () { StopAllCoroutines(); StartCoroutine(Startup()); };
 		Module.GetComponent<KMSelectable>().OnFocus += delegate { moduleSelected = true; };
 		Module.GetComponent<KMSelectable>().OnDefocus += delegate { moduleSelected = false; };
@@ -164,24 +183,203 @@ public class SetupWizardScript : MonoBehaviour {
 		username = new ObtainUsername(foldersToShuffle.Select(x => folders[x]).ToArray(), folders, folders[currentPos]);
 
 		Log($"[Setup Wizard #{moduleId}] The swapped folders for step 1 were: {foldersToShuffle.Select(x => folders[x].FolderName).Join(", ")}");
+		Log($"[Setup Wizard #{moduleId}] The username should be {username.GetUsername(Bomb)}");
 
+		passwordDigits = equationSystem.GeneratedPassword();
+
+		var randomIxes = new int[6];
+
+		for (int i = 0; i < 6; i++)
+			randomIxes[i] = Range(0, 5);
+
+		generatedPuzzle = equationSystem.GeneratedPuzzle(passwordDigits, randomIxes);
+		modifiedPuzzle = generatedPuzzle;
+
+		var answersToShuffle = Enumerable.Range(0, 6).ToList().Shuffle().Take(2).ToArray();
+
+		var shuffledAnswers = SwapAnswers(answersToShuffle);
+
+		for (int i = 0; i < 2; i++)
+			modifiedPuzzle[answersToShuffle[i]] = shuffledAnswers[i];
 
     }
 
+	void AccountPromptPress(KMSelectable prompt)
+	{
+		prompt.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("Click", transform);
+
+		if (moduleSolved || !isActivated || !canSubmit)
+			return;
+
+		switch (Array.IndexOf(accountPrompts, prompt))
+		{
+			case 0:
+				if (canTypeUser)
+					return;
+
+				canTypeUser = !canTypeUser;
+				break;
+			case 1:
+				if (canTypePassword)
+					return;
+
+				canTypePassword = !canTypePassword;
+				break;
+		}
+	}
+
+	void MainButtonPress(KMSelectable button)
+	{
+		button.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("Click", transform);
+
+		if (moduleSolved || !isActivated)
+			return;
+
+		switch (Array.IndexOf(mainButtons, button))
+		{
+			case 0:
+				switch (currentPage)
+				{
+					case 0:
+						return;
+					default:
+						currentPage--;
+						break;
+				}
+				break;
+			case 1:
+				switch (currentPage)
+				{
+					default:
+						currentPage++;
+						break;
+					case 0:
+						FolderUpdate();
+						goto default;
+					case 1:
+
+						goto default;
+					case 3:
+						if (usernameInput == username.GetUsername(Bomb) && passwordInput == passwordDigits.Join(""))
+						{
+							Log($"[Setup Wizard #{moduleId}] The username and password are correct. Moving over to the finish screen");
+							currentPage++;
+						}
+						else
+						{
+							Log($"[Setup Wizard #{moduleId}] Either the username or password is invalid. Strike!");
+							Module.HandleStrike();
+						}
+						break;
+					case 4:
+						currentPage++;
+						canSolve = true;
+						break;
+					case 5:
+						return;
+				}
+				break;
+		}
+
+		WindowUpdate();
+	}
+
+	void WindowUpdate()
+	{
+		var mainText = mainButtons[1].GetComponentInChildren<TextMesh>().text;
+
+		mainButtons[1].GetComponentInChildren<TextMesh>().text = currentPos == 4 ? "Finish" : mainText;
+
+		if (currentPage == 5)
+		{
+			foreach (KMSelectable mainButton in mainButtons)
+				mainButton.gameObject.SetActive(false);
+			reset.gameObject.SetActive(false);
+
+			foreach (var obj in pages)
+				obj.SetActive(false);
+
+			windowIcon.material = windowIcons[1];
+			submit.gameObject.SetActive(true);
+
+			return;
+		}
+
+		
+
+		for (int i = 0; i < pages.Length; i++)
+			pages[i].SetActive(i == currentPage);
+	}
+
+	void FolderPress(KMSelectable folder)
+	{
+		folder.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("Click", transform);
+
+		if (moduleSolved || !isActivated || currentPage != 1)
+			return;
+
+		var ix = Array.IndexOf(folderButtons, folder);
+
+		currentPos = shuffledFolders[currentPos].Directories == null ? shuffledFolders[currentPos].SingleDirectory.Value : shuffledFolders[currentPos].Directories[ix];
+
+		FolderUpdate();
+	}
+
+	void FolderUpdate()
+	{
+
+		if (shuffledFolders[currentPos].Directories == null)
+		{
+			folderButtons[1].gameObject.SetActive(false);
+			folderButtons[0].GetComponentInChildren<TextMesh>().text = shuffledFolders[folders[currentPos].SingleDirectory.Value].FolderName;
+		}
+		else
+		{
+			folderButtons[1].gameObject.SetActive(true);
+
+			var folderNames = shuffledFolders[currentPos].Directories.Select(x => folders[x].FolderName).ToArray();
+
+			for (int i = 0; i < 2; i++)
+				folderButtons[i].GetComponentInChildren<TextMesh>().text = folderNames[i];
+		}
+
+	}
+
+	void Page2Update()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			var ix = i + page2Ix;
+			expressionDisplays[i].text = $"{"a),b),c),d),e),f)".Split(',')[ix]} {modifiedPuzzle[ix].NumIxA} {modifiedPuzzle[ix].EquationExpression} {modifiedPuzzle[ix].NumIxB} = {modifiedPuzzle[ix].Answer}";
+        }
+			
+	}
+
+
 	void KeyboardLetterPress(KMSelectable letter)
 	{
+		letter.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("KeyPress", transform);
+
 		if (moduleSolved || !isActivated || !canSubmit && !canTypeUser)
 			return;
 
 		if (usernameInput.Length < 10)
 		{
             usernameInput += letter.GetComponentInChildren<TextMesh>().text;
+			accountPrompts[0].GetComponentInChildren<TextMesh>().text = usernameInput;
         }
 			
 	}
 
 	void KeyboardNumberPress(KMSelectable number)
 	{
+		number.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("KeyPress", transform);
+
 		if (moduleSolved || !isActivated || !canSubmit && !canTypePassword)
 			return;
 
@@ -189,12 +387,16 @@ public class SetupWizardScript : MonoBehaviour {
 		{
             passwordInput += number.GetComponentInChildren<TextMesh>().text;
 			passwordAsterisk += '*';
+			accountPrompts[1].GetComponentInChildren<TextMesh>().text = passwordAsterisk;
         }
 			
 	}
 
 	void ShiftPress()
 	{
+		shift.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("KeyPress", transform);
+
 		if (moduleSolved || !isActivated || !canSubmit)
 			return;
 
@@ -206,13 +408,39 @@ public class SetupWizardScript : MonoBehaviour {
 			keyboardLetters[i].GetComponentInChildren<TextMesh>().text = shiftedLetters ? letters[i].ToString().ToUpperInvariant() : letters[i].ToString().ToLowerInvariant();
 	}
 
+	void SubmitPress()
+	{
+		submit.AddInteractionPunch(0.4f);
+		Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+
+		if (moduleSolved || !isActivated || !canSolve)
+			return;
+
+		Audio.PlaySoundAtTransform("Solve", transform);
+
+		Log($"[Setup Wizard #{moduleSolved}] The button has been pressed. Solved!");
+
+		moduleSolved = true;
+		Module.HandlePass();
+	}
+
 	void ResetPress()
 	{
+		reset.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("Click", transform);
 
+		if (moduleSolved || !isActivated || currentPage == 0)
+			return;
+
+		currentPage = 0;
+		WindowUpdate();
 	}
 
 	void BackSpacePress()
 	{
+		backSpace.AddInteractionPunch(0.4f);
+		Audio.PlaySoundAtTransform("KeyPress", transform);
+
 		if (moduleSolved || !isActivated || !canSubmit)
 			return;
 
@@ -220,14 +448,16 @@ public class SetupWizardScript : MonoBehaviour {
 			if (usernameInput.Length > 0)
 			{
 				usernameInput.Remove(usernameInput.Length - 1);
-			}
+                accountPrompts[0].GetComponentInChildren<TextMesh>().text = passwordAsterisk;
+            }
 		
 		else if (canTypePassword)
 				if (passwordInput.Length > 0)
 				{
 					passwordInput.Remove(passwordInput.Length - 1);
 					passwordAsterisk.Remove(passwordAsterisk.Length - 1);
-				}
+                    accountPrompts[1].GetComponentInChildren<TextMesh>().text = passwordAsterisk;
+                }
 	}
 
 	IEnumerator Startup()
